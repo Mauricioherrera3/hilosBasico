90bb21cc20e2e38db9d89a3cec03ac1478edadf4
Mauricioherrera3 <Laboratorio@ULAT-PZ-LB1-03>
2024-10-02 21:16:47 -0600

    Ejemplo hilo

--- hilosBasico/Program.cs
+++ hilosBasico/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace hilosBasico
@@ -10,6 +11,36 @@ namespace hilosBasico
     {
         static void Main(string[] args)
         {
+            Thread hilo1 = new Thread(FuncionHilo);
+            Thread hilo2 = new Thread(FuncionHilo2);
+
+            hilo1.Start();
+            hilo2.Start();
+
+
         }
+        public static void FuncionHilo()
+        {
+            for (int i = 0; i < 5; i++) { 
+            Console.WriteLine("hILO pRINCIPAL"+i);
+            Thread.Sleep(1000); 
+            
+            
+            }
+
+        }
+
+        public static void FuncionHilo2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("hilo Secundario" + i);
+                Thread.Sleep(1000);
+
+
+            }
+
+        }
+
     }
 }
